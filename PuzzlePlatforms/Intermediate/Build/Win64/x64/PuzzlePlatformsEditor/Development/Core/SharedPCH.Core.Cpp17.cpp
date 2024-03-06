8199fdb60cb1436d0642ae379d5102b9c5a68e80
Animesh Sharma <animeshsharma1996@gmail.com>
2024-03-06 14:13:01 +0000

    Correct GitIgnore

--- PuzzlePlatforms/Intermediate/Build/Win64/x64/PuzzlePlatformsEditor/Development/Core/SharedPCH.Core.Cpp17.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32545
-#include "D:/UETutMP/PuzzlePlatforms/Intermediate/Build/Win64/x64/PuzzlePlatformsEditor/Development/Core/SharedPCH.Core.Cpp17.h"
--- PuzzlePlatforms/Intermediate/Build/Win64/x64/PuzzlePlatformsEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/UETutMP/PuzzlePlatforms/Intermediate/Build/Win64/x64/PuzzlePlatformsEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
